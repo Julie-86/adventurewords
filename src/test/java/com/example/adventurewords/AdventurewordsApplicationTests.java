eacf6f530023b4660e758aeb76e8dcc85b68f211
Iuliana Necula <iuliana.necula86@gmail.com>
2026-03-24 14:05:42 +0200

    chore: add testing setup and Maven wrapper

---
+++ src/test/java/com/example/adventurewords/AdventurewordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventurewords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurewordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
